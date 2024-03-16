5ee6b9ef6094a93798df4e772111231e17aa55ab
Geronimo Ferrario <ferrario.dev@gmail.com>
2024-03-16 13:00:43 +0000

    feat: login system finished, models for account and transactions

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length, EqualTo
 from wtforms import StringField, PasswordField, SubmitField
+from datetime import datetime
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -25,6 +26,23 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    balance = db.Column(db.Integer, default=2000)
+    user = db.relationship('User', backref=db.backref('account', lazy=True))
+
+    def __repr__(self):
+        return f'<Account {self.id}>'
+    
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime)
+    account = db.relationship('Account', backref=db.backref('transaction', lazy=True))
     
 class registration_form(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -53,8 +71,18 @@ def register():
         password = form.password.data
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
+        # Check if the username already exists in the database
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
+        db.session.commit()
+        
+        new_user_account = Account(user_id=new_user.id)
+        db.session.add(new_user_account)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('auth.html', title = 'Register', text='Create an account', form=registration_form(), btn_action="Register")
@@ -67,15 +95,21 @@ def login():
         password = form.password.data
         user = User.query.filter_by(username=username).first()
 
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('dashboard'))
+        if user:
+            if bcrypt.check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('home'))
+            else:
+                user.login_attempts += 1
+                db.session.commit()
+                flash('Invalid password')
+                if user.login_attempts > 3:
+                    user.is_locked = True
+                    db.session.commit()
+                    flash('Your account is locked')
         else:
-            user.login_attempts += 1
-            flash('Invalid username or password')
-            if user.login_attempts > 3:
-                user.is_locked = True
-                flash('Your account is locked')
+            flash('Invalid username')
+
     return render_template('auth.html', title='Login', text='Login to your account', form=registration_form(),btn_action="Login")
 
 @app.route('/logout')
